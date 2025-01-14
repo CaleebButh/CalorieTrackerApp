2bc6cf7fb6035cbf201716475a88335903d2be3e
Caleb Buth <Caleb.buth@gmail.com>
2025-01-14 17:08:37 -0600

    Added pie chart rendering.
    pie chart renders on top of bar graph so we will have to figure that one out.

--- main.py
+++ main.py
@@ -28,7 +28,7 @@ while not done:
     if choice == "1":
         print("Adding a new food.")
         name = input("Name: ")
-        protein = input("Protein: ")
+        protein = int(input("Protein: "))
         calories = int(input("Calories: "))
         carbs = int(input("Carbs: "))
         fat = int(input("Fat: "))
@@ -42,23 +42,36 @@ while not done:
             print("No foods added yet!")
             continue
 
-        #Continue
-       # total_protein = sum(food.protein for food in today)
-        #total_calories = sum(food.calories for food in today)
-        #total_carbs = sum(food.carbs for food in today)
-        #total_fat = sum(food.fat for food in today)
+        #Generates Macro pie chart
+        total_protein = sum(food.protein for food in today)
+        total_calories = sum(food.calories for food in today)
+        total_carbs = sum(food.carbs for food in today)
+        total_fat = sum(food.fat for food in today)
 
+        #Generates pie chart
+        macros = [total_calories, total_carbs, total_fat, total_protein]
+        pieChartLabels = ["Calories", "Carbs", "Fats", "Protein"]
+
+        fig, ax = plt.subplots()
+        ax.pie(macros, labels=pieChartLabels, autopct="%1.1f%%")
+
+        #adds calories from foods to list.
         foodsToday = [food.name for food in today]
-        calsToday = [food.calories for food in today]
+        calsToday = []
+        for food in today:
+            calsToday.append(food.calories)
 
+        #Generates caloric bar graph
         plt.bar(foodsToday, calsToday, width=0.3)
         plt.title("Calories from food today.")
         plt.xlabel("Foods")
         plt.ylabel("Calories")
+
+        #show plots
         plt.show()
 
-
-
-    if choice == "q":
-        print("Quitting program")
-        break
+    elif choice.lower() == "q":
+        done = True
+    else:
+        continue
+print("Goodbye")
